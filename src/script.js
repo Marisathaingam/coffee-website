1c1695ca548fe9f0e1be0e6e55867a124c5925b5
Marisathaingam <marisathaingamgoi14@gmail.com>
2024-11-22 22:02:35 +0100

    coffeewebsite

---
+++ src/script.js
@@ -0,0 +1,12 @@
+const menuOpenButton = document.querySelector ("#menu-open-button");
+const menuCloseButton = document.querySelector ("#menu-close-button");
+const navMenu = document .querySelector(".nav-menu");
+
+menuOpenButton.addEventListener("click", () =>{
+  // TOGGLE MOBILE MENU VISIBILLITY //
+   document.body.classList.toggle("show-mobile-menu");
+});
+
+// CLOSE MENU WHEN CLOSE BUTTON IS CLICKED //
+menuCloseButton.addEventListener("click", () => menuOpenButton.click ());
+   
