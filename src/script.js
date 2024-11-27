bc3822503747346659986feba53b79ca5d9e5e3d
Marisathaingam <marisathaingamgoi14@gmail.com>
2024-11-27 19:42:16 +0100

    coffee projact

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ const menuOpenButton = document.querySelector ("#menu-open-button");
 const menuCloseButton = document.querySelector ("#menu-close-button");
 const navMenu = document .querySelector(".nav-menu");
 
-menuOpenButton.addEventListener("click", () =>{
+menuOpenButton.addEventListener("click", () => {
   // TOGGLE MOBILE MENU VISIBILLITY //
    document.body.classList.toggle("show-mobile-menu");
 });
