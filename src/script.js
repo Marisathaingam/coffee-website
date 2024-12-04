48097ba0a8ed5faf35e58b39180c222ebc733d22
Marisathaingam <marisathaingamgoi14@gmail.com>
2024-12-04 21:20:22 +0100

    marisacoffee

--- src/script.js
+++ src/script.js
@@ -9,4 +9,38 @@ menuOpenButton.addEventListener("click", () => {
 
 // CLOSE MENU WHEN CLOSE BUTTON IS CLICKED //
 menuCloseButton.addEventListener("click", () => menuOpenButton.click ());
+
+//INTIALIZE SWIPER //
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // IF WE NEED PAGINATION //
+  pagination: {
+    el: '.swiper-pagination', 
+    clickable:true,
+    dynamicBullets: true,
+
+  },
+
+  // NAVIGATION ARROWS //
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // RESPONSIVE BRWAKPOINTS //
+  breakpoints: {
+    0:{
+      slidesPerView: 1
+    },
+    768:{
+      slidesPerView: 2 
+    },
+    1024:{
+      slidesPerView: 3
+    } 
+  }
+});
    
